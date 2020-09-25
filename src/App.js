9db7dc0b756274cebb605f7cd5f5e6b04c3ad826
Nidvar <djayshine87@hotmail.com>
2020-09-25 14:41:01 +1000

    project summary and details

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Dashboard from './components/dashboard/Dashboard'
+import Dashboard from './components/dashboard/Dashboard';
+import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Navbar />
         <h1>NN React Redux Firebase</h1>
         <Switch>
-          <Route path='/' component ={Dashboard} />
+          <Route exact path='/' component ={Dashboard} />
+          <Route path='/projects/:id' component = {ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
