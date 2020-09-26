e5af918edfe11aa01282163f2cd7d3304c8a3b08
Nidvar <djayshine87@hotmail.com>
2020-09-26 11:47:45 +1000

    signin and signup forms

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
-import Signin from './components/auth/SignIn';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Switch>
           <Route exact path='/' component ={Dashboard} />
           <Route path='/projects/:id' component = {ProjectDetails} />
-          <Route exact path='/signin' component={Signin} />
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
 class SignIn extends React.Component{
+    state={
+        email:'',
+        password:''
+    }
+    handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.id]: e.target.value,
+        })
+    }
+
     render(){
         return(
             <div>
                 <h1>Sign In</h1>
-                <form className='Sign_in'>
-                    <input type='email' id='email' placeholder='email' /><br />
-                    <input type='password' id='password'  placeholder='password' /><br />
+                <form className='Sign_in' onSubmit={this.handleSubmit}>
+                    <input type='email' id='email' placeholder='email' onChange={this.handleChange} /><br />
+                    <input type='password' id='password'  placeholder='password' onChange={this.handleChange} /><br />
                     <button>Login</button>
                 </form>
             </div>
-            
         )
     }
 }
